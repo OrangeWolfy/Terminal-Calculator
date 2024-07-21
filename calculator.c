3affff3c1024f7fb09056e9fe016b41e36249c97
OrangeWolfy <orange007wolfy@gmail.com>
2024-07-21 01:59:25 -0600

    Added comments

--- calculator.c
+++ calculator.c
@@ -8,6 +8,37 @@
 // Terminal calculator as alias command
 // Execute this file and create an alias to use anywhere
 
+int operator_precedence(char arg);
+struct queue *infix_to_postfix(char arg[]);
+long double evaluate_postfix(struct queue **output_queue);
+
+int main(int argc, char* argv[]) {
+	// Check if no arguments were given
+	if(argc < 2) {
+		return -1;
+	}
+	// Print help menu if requested
+	if(strcmp(argv[1], "--h") == 0 || strcmp(argv[1], "--help") == 0) {
+		printf("Usage: calc [OPERATION]\naddition, subtraction, multiplication, division and or modulus.\n");
+        printf("Solve the [OPERATION] given.\n");
+        printf("The [OPERATION] given must only contain:\n");
+        printf("\t-numbers\n\t-addition symbol +\n\t-subtraction symbol -\n\t-multiplication symbol * or x\n\t-division symbol /\n\t-modulus symbol %%\n\t-open and closed parentheses ( )\n");
+        printf("\nFor more information: <https://github.com/OrangeWolfy/Terminal-Calculator>\n");
+		return 0;
+	}
+
+	struct queue *output_queue;
+	output_queue = infix_to_postfix(argv[1]);
+	if(output_queue == NULL) {
+		return -1;
+	}
+
+	printf("%.3Lf\n", evaluate_postfix(&output_queue));
+
+    return 0;
+}
+
+// Return the value of precedence
 int operator_precedence(char arg) {
 	int operator_val;
 
@@ -34,13 +65,16 @@ int operator_precedence(char arg) {
 	return operator_val;
 }
 
+// Transform a infix to postfix operation with the shunting yard algorithm and return the transformation in a queue
 struct queue *infix_to_postfix(char arg[]) {
 	struct queue *output_queue = NULL;
 	struct stack_char *operator_stack = NULL;
 	long double num = 0;
 	int precedence, parentheses_flag = 0;
 
+	// Loop until argument is empty or an error occurs
 	for(int i = 0; arg[i] != '\0'; i++) {
+		// Check if the current char is a number or open parentheses
 		if(((int)arg[i] >= 48 && (int)arg[i] <= 57) || arg[i] == '(') {
 			if(arg[i] != '(') {
 				num += arg[i] - 48;
@@ -48,6 +82,7 @@ struct queue *infix_to_postfix(char arg[]) {
 			} else {
 				push_char(&operator_stack, arg[i]);
 			}
+		// Check if the current char is valid
 		} else if ((precedence = operator_precedence(arg[i])) != -1) {
 			num /= 10;
 			if(enqueue(&output_queue, num, '\0') == -1) {
@@ -58,6 +93,7 @@ struct queue *infix_to_postfix(char arg[]) {
 
 			char c;
 			if(arg[i] != ')') {
+				// Empty operator stack while the current operator has lower or equal precedence to the top of the operator stack
 				while(operator_stack != NULL && precedence <= operator_precedence(operator_stack->op)) {
 					c = pop_char(&operator_stack);
 					if(c == '\0') {
@@ -70,6 +106,7 @@ struct queue *infix_to_postfix(char arg[]) {
 					}
 				}
 			} else {
+				// Empty operator stack until open parentheses is found
 				while(operator_stack != NULL) {
 					c = pop_char(&operator_stack);
 					if(c == '(') {
@@ -103,6 +140,7 @@ struct queue *infix_to_postfix(char arg[]) {
 	}
 	num = 0;
 
+	// Empty operator stack to output queue
 	while(operator_stack != NULL) {
 		char op = pop_char(&operator_stack);
 		if(enqueue(&output_queue, -1, op) == -1) {
@@ -119,17 +157,21 @@ cleanup:
 	return NULL;
 }
 
+// Evaluate the postfix operation in queue and return the final answer for the operation
 long double evaluate_postfix(struct queue **output_queue) {
 	struct stack_longdouble *solve_stack = NULL;
 	long double num1, num2;
 
+	// Loop until output queue is empty or an error occurs
 	while(*output_queue != NULL) {
+		// Push to the solved stack if it is a number
 		if((*output_queue)->op == '\0') {
 			if (push_longdouble(&solve_stack, (*output_queue)->val) == -1) {
 				printf("Fail to push number in line %d\n", __LINE__);
 				goto cleanup;
 			}
 		} else {
+			// If the next queue is an operator, pop the first two numbers from the solved stack, evaluate below and push the result
 			num2 = pop_longdouble(&solve_stack); 
 			if(num2 == -1) {
 				printf("Fail to pop, number stack empty in line %d\n", __LINE__);
@@ -192,38 +234,3 @@ cleanup:
 	return -1;
 }
 
-void print_output_queue(struct queue *output_queue) {
-	while(output_queue != NULL) {
-		if(output_queue->val != -1)
-			printf("%.3Lf ", output_queue->val);
-		else printf("%c ", output_queue->op);
-		output_queue = output_queue->next;
-	}
-	printf("\n");
-}
-
-int main(int argc, char* argv[]){
-	if(argc < 2) {
-		return -1;
-	}
-	if(strcmp(argv[1], "--h") == 0 || strcmp(argv[1], "--help") == 0) {
-		printf("Usage: calc [OPERATION]\naddition, subtraction, multiplication, division and or modulus.\n");
-        printf("Solve the [OPERATION] given.\n");
-        printf("The [OPERATION] given must only contain:\n");
-        printf("\t-numbers\n\t-addition symbol +\n\t-subtraction symbol -\n\t-multiplication symbol * or x\n\t-division symbol /\n\t-modulus symbol %%\n\t-open and closed parentheses ( )\n");
-        printf("\nFor more information: <https://github.com/OrangeWolfy/Terminal-Calculator>\n");
-		return 0;
-	}
-
-	struct queue *output_queue;
-	output_queue = infix_to_postfix(argv[1]);
-	if(output_queue == NULL) {
-		return -1;
-	}
-	
-	print_output_queue(output_queue);
-
-	printf("%.3Lf\n", evaluate_postfix(&output_queue));
-
-    return 0;
-}
